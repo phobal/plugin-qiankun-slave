f97fe679c3b12d7db9e4a5eaa1243fee37277a78
Phobal <fengb@chinawyny.com>
2019-11-01 16:38:17 +0800

    :sparkles: init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// ref:
+// - https://umijs.org/plugin/develop.html
+
+export default function(api, options) {
+  // Example: output the webpack config
+  api.chainWebpackConfig((config) => {
+    console.log(config.toString())
+  })
+}
