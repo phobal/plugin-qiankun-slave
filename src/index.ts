3532112fb13ef78e3249ba5df48b2846ad8a4494
Phobal <phobal@126.com>
2019-11-06 17:40:58 +0800

    feat: add define

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // ref:
 // - https://umijs.org/plugin/develop.html
-
+import { IApi } from 'umi-plugin-types';
 interface Options {
   name: string
 }
 
-export default function(api, options: Options) {
+export default function(api: IApi, options: Options) {
   const { name } = options
   api.modifyDefaultConfig((memo) => {
     return {
@@ -15,6 +15,7 @@ export default function(api, options: Options) {
       publicPath: `/${name}/`,
       mountElementId: `${name}`,
       define: {
+        // @ts-ignore
         ...memo.define,
         API_PREFIX: `/api/${name}`
       },
