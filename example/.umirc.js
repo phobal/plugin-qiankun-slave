189d501cb0000e3befd4cd916b2f59471caae11d
Phobal <phobal@126.com>
2020-05-07 17:21:30 +0800

    update example

--- example/.umirc.js
+++ example/.umirc.js
@@ -3,23 +3,7 @@ import { join } from 'path'
 export default {
   routes: [{ path: '/', component: './index' }],
   plugins: [
-    [
-      'umi-plugin-react',
-      {
-        title: '测试',
-        antd: true,
-        dva: { hmr: true, immer: true },
-        dynamicImport: {
-          webpackChunkName: true,
-          level: 3
-        },
-        chunks: ['umi'],
-        dll: true
-      }
-    ],
-    [
-      join(__dirname, '..', require('../package').src || 'index.js'),
-      { isShowButton: false, portalDomain: 'http://172.16.11.30:5000' }
-    ]
-  ]
+      join(__dirname, '..', require('../package.json').src || 'index.js'),
+  ],
+  'qiankun-slave': { portalDomain: 'http://172.16.11.30:5000' }
 }
